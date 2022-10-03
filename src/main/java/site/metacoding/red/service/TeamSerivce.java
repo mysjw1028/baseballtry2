191cae2eda8a0a26c0cf063ba7cfe60ee262c0d0
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-10-03 13:42:56 +0900

    팀 등록완료

--- src/main/java/site/metacoding/red/service/TeamSerivce.java
+++ src/main/java/site/metacoding/red/service/TeamSerivce.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+
 import site.metacoding.red.domain.team.Team;
 import site.metacoding.red.domain.team.TeamDao;
+import site.metacoding.red.web.dto.team.TeamInsertReqDto;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +18,8 @@ public class TeamSerivce {
 
 	public List<Team> 목록보기() {
 		return teamDao.findAll();
+	}
+	public void 팀등록(TeamInsertReqDto teamInsertReqDto) {
+		teamDao.insert(teamInsertReqDto.toEntitly());
 	}
 }
--- src/main/java/site/metacoding/red/web/TeamController.java
+++ src/main/java/site/metacoding/red/web/TeamController.java
@@ -5,16 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.stadium.Stadium;
 import site.metacoding.red.domain.team.Team;
+import site.metacoding.red.service.StadiumService;
 import site.metacoding.red.service.TeamSerivce;
+import site.metacoding.red.web.dto.CMResponse;
+import site.metacoding.red.web.dto.team.TeamInsertReqDto;
 
 @RequiredArgsConstructor
 @Controller
 public class TeamController {
 	
 	private final TeamSerivce teamSerivce;
+	private final StadiumService stadiumService;
 
 		@GetMapping("/team")
 		public String list(Model model) {
@@ -22,4 +30,17 @@ public class TeamController {
 			model.addAttribute("teamList",teamList);
 			return "team/list";
 		}
+		
+		@GetMapping("/teamForm")
+		public String teamForm(Model model) {
+			List<Stadium> stadiumList = stadiumService.목록보기();
+			model.addAttribute("stadiumList",stadiumList);
+			return "team/saveForm";
+		}
+		@PostMapping("/team")
+		public @ResponseBody CMResponse<?>insert(@RequestBody TeamInsertReqDto teamInsertReqDto){
+			teamSerivce.팀등록(teamInsertReqDto);
+			return new CMResponse<>(1, "팀등록 성공", null);
+		}
+
 }
